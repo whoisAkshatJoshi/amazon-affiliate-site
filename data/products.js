99ef83647c60b8429eb9f62392be1f935d92c65d
Akshat Joshi <akshatjoshi.aj2001@gmail.com>
2026-01-03 10:58:42 +0530

    Initial affiliate website

--- data/products.js
+++ data/products.js
@@ -1,10 +1,19 @@
 const products = [
   {
-    slug: " Wireless Mini Portable Bluetooth Speaker with Microphone & LED Lights",
+    slug: "Wireless Mini Portable Bluetooth Speaker with Microphone & LED Lights",
     name: "Wembley Rechargeable Karaoke Mic with Speaker for Singing",
     description: "Experience powerful and crystal-clear audio with our mini karaoke machine. Equipped with dual speakers, it delivers deep bass and crisp treble, creating a captivating three-dimensional effect. Plus, the built-in LED light enhances the singing atmosphere.",
     image: "https://m.media-amazon.com/images/I/61+rs1F+oAL._SX522_.jpg",
     link: "https://www.amazon.in/Wembley-Rechargeable-Wireless-Bluetooth-Microphone/dp/B0DRPCR5VK/ref=zg_bs_c_toys_d_sccl_3/258-0330635-3740842?pd_rd_w=EeUeI&content-id=amzn1.sym.b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_p=b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_r=MB4E1VAKNE7Y9KRKBANS&pd_rd_wg=kJYi3&pd_rd_r=d9be987d-b04d-41c1-905b-2b7293e059ed&pd_rd_i=B0DRPCR5VK&th=1"
+  },
+];
+[
+  {
+    slug: "HNY Digital Alarm Clock with Automatic Sensor, Date & Temperature Display, Compact LED Desk Table Clock for Students, Bedroom, Office, Living Room, Home Decor",
+    name: "HNY Digital Alarm Clock",
+    description: "DISPLAY FEATURES: Large LCD screen shows time in 12/24-hour format, date, temperature in °C/°F, and includes smart backlight with automatic sensor. COMPACT SIZE: Measures 13.5 cm in length, 7.6 cm in height, and 5 cm in depth, perfect for bedside tables and office desks. VERSATILE FUNCTIONS: Equipped with alarm function, snooze option, and adjustable display brightness for comfortable viewing. TEMPERATURE DISPLAY: Built-in temperature sensor shows ambient temperature, easily switchable between Celsius and Fahrenheit. EASY OPERATION: Convenient button layout for simple time/date setting, alarm configuration, and display mode adjustment",
+    image: "https://m.media-amazon.com/images/I/51E69SqCZoL._SX679_.jpg",
+    link: "https://www.amazon.in/HNY-Digital-Automatic-Temperature-Students/dp/B0FSDKZVR7/ref=zg_bsnr_c_kitchen_d_sccl_12/258-0330635-3740842?pd_rd_w=CKYCZ&content-id=amzn1.sym.b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_p=b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_r=XR81WZ61GA4TFEK8ZPM6&pd_rd_wg=06vSs&pd_rd_r=2b2276e9-e903-4d81-a4e8-29525d592e72&pd_rd_i=B0FSDKZVR7&psc=1"
   },
 ];
 
