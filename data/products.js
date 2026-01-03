c8bbdfed2d0c14d008d7ebb5ee1265efbc668c8f
Akshat Joshi <akshatjoshi.aj2001@gmail.com>
2026-01-03 10:58:42 +0530

    Initial affiliate website

--- data/products.js
+++ data/products.js
@@ -1,10 +1,10 @@
 const products = [
   {
-    slug: "wireless-headphones",
-    name: "Wireless Bluetooth Headphones",
-    description: "Noise cancellation, 40h battery, deep bass.",
-    image: "https://via.placeholder.com/300",
-    link: "YOUR_AMAZON_AFFILIATE_LINK"
+    slug: " Wireless Mini Portable Bluetooth Speaker with Microphone & LED Lights",
+    name: "Wembley Rechargeable Karaoke Mic with Speaker for Singing",
+    description: "Experience powerful and crystal-clear audio with our mini karaoke machine. Equipped with dual speakers, it delivers deep bass and crisp treble, creating a captivating three-dimensional effect. Plus, the built-in LED light enhances the singing atmosphere.",
+    image: "https://m.media-amazon.com/images/I/61+rs1F+oAL._SX522_.jpg",
+    link: "https://www.amazon.in/Wembley-Rechargeable-Wireless-Bluetooth-Microphone/dp/B0DRPCR5VK/ref=zg_bs_c_toys_d_sccl_3/258-0330635-3740842?pd_rd_w=EeUeI&content-id=amzn1.sym.b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_p=b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_r=MB4E1VAKNE7Y9KRKBANS&pd_rd_wg=kJYi3&pd_rd_r=d9be987d-b04d-41c1-905b-2b7293e059ed&pd_rd_i=B0DRPCR5VK&th=1"
   },
 ];
 
