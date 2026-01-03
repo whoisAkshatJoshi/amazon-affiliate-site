8a387d594d091fb34f38f4cf507bc7ffc2f0f909
Akshat Joshi <akshatjoshi.aj2001@gmail.com>
2026-01-03 10:58:42 +0530

    Initial affiliate website

--- data/products.js
+++ data/products.js
@@ -6,8 +6,7 @@ const products = [
     image: "https://m.media-amazon.com/images/I/61+rs1F+oAL._SX522_.jpg",
     link: "https://www.amazon.in/Wembley-Rechargeable-Wireless-Bluetooth-Microphone/dp/B0DRPCR5VK/ref=zg_bs_c_toys_d_sccl_3/258-0330635-3740842?pd_rd_w=EeUeI&content-id=amzn1.sym.b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_p=b908f532-cbe7-4274-8b24-b671acc58bd2&pf_rd_r=MB4E1VAKNE7Y9KRKBANS&pd_rd_wg=kJYi3&pd_rd_r=d9be987d-b04d-41c1-905b-2b7293e059ed&pd_rd_i=B0DRPCR5VK&th=1"
   },
-];
-[
+
   {
     slug: "HNY Digital Alarm Clock with Automatic Sensor, Date & Temperature Display, Compact LED Desk Table Clock for Students, Bedroom, Office, Living Room, Home Decor",
     name: "HNY Digital Alarm Clock",
